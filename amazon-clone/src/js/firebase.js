002107232f65de84f2b35f21a5e3f6f6f994821b
Ankur Mishra <ankurmishra@Ankurs-MacBook-Air.local>
2021-03-09 23:52:26 -0800

    2. added home page functionality

---
+++ amazon-clone/src/js/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD0SajhWrzhfpxwXtjo8oaA1Mu1Me4JSpE",
+    authDomain: "clone-test-1a208.firebaseapp.com",
+    projectId: "clone-test-1a208",
+    storageBucket: "clone-test-1a208.appspot.com",
+    messagingSenderId: "951975800548",
+    appId: "1:951975800548:web:ed3856d5dc8cb2284246f1",
+    measurementId: "G-SH9T3MXZJE"
+  };
